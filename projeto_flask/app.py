d43011d1bdb112be358d7e3af7f28203f0e98fbc
MobPudim <carloshkz157@gmail.com>
2025-10-27 21:03:51 -0400

    app v0.3.1 - criação de um novo arquivo html e atualização do codigo app.py

--- projeto_flask/app.py
+++ projeto_flask/app.py
@@ -1,26 +1,39 @@
 from flask import Flask, render_template
 
-meu_app = Flask(__name__ , template_folder='templates')
-
+meu_app = Flask(__name__)
+ 
 
 @meu_app.route("/")  
-def homepage():         
+def homepage():    
     return render_template ("homepage.html")
-
-@meu_app.route("/contato")
-def contato():
-    return render_template("contato.html") 
 
 @meu_app.route("/index")
 def indice():
     return render_template ("index.html") 
 
+@meu_app.route("/contato")
+def contato():
+    return render_template("contato.html") 
+
 @meu_app.route("/usuario")
 def dados_usuario():
+ 
+    dados_usu = {"nome": "Mariela","profissao": "Professora EBTT", "disciplina":"Desenvolvimento Web III"}
+    return render_template("usuario.html",  dados = dados_usu)
 
-    dados_usu = {"nome": "Neymar", "profissao": "Jogador", "disciplina":"Desenvolvimento Web III"}
-    return render_template("usuario.html", dados = dados_usu)
+@meu_app.route('/usuario/<id>')
+def saudacao(id):
+ 
+    return render_template('homepage_nome.html', nome=id)
+
+@meu_app.route("/usuario/<nome_usuario>;<nome_profissao>;<nome_disciplina>") 
+
+def usuario (nome_usuario, nome_profissao, nome_disciplina):
+    
+    dados_usu = {"profissao": nome_profissao, "disciplina": nome_disciplina}
+
+
+    return render_template ("usuario.html", nome=nome_usuario, dados = dados_usu)  
 
 if __name__ == "__main__": 
      meu_app.run(port = 8000) 
-                                
