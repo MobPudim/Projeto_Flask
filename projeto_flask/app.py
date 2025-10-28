8d25cd1e6c75d33517cc985dbd20b9a8b6d3659d
MobPudim <carloshkz157@gmail.com>
2025-10-27 20:42:13 -0400

    app v0.3.0 - criação da pagina index e usuario e correcao de bugs

--- projeto_flask/app.py
+++ projeto_flask/app.py
@@ -11,6 +11,16 @@ def homepage():
 def contato():
     return render_template("contato.html") 
 
+@meu_app.route("/index")
+def indice():
+    return render_template ("index.html") 
+
+@meu_app.route("/usuario")
+def dados_usuario():
+
+    dados_usu = {"nome": "Neymar", "profissao": "Jogador", "disciplina":"Desenvolvimento Web III"}
+    return render_template("usuario.html", dados = dados_usu)
+
 if __name__ == "__main__": 
      meu_app.run(port = 8000) 
                                 
