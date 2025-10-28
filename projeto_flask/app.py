1a01e3ae83a9fc0e98c48e8c233c05a51c3a8769
MobPudim <carloshkz157@gmail.com>
2025-10-27 20:22:41 -0400

    app v0.1.0 - criação de rotas e def

--- projeto_flask/app.py
+++ projeto_flask/app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask  
 
-app_Mariela = Flask (__name__)
+meu_app = Flask (__name__) 
 
-@app_Mariela.route('/')
-def raiz():
+@meu_app.route('/')    
+@meu_app.route('/rota1') 
+def rota1():  
     return 'Olá, turma!'
 
-app_Mariela.run()
+@meu_app.route('/rota2')
+def rota2():
+    resposta = "<H3> Essa é outra página da rota 2 <H3>"
+    return resposta
+ 
+def saudacoes (nome): 
+    return f'Olá, {nome}'
+
+if __name__ == "__main__" :
+    meu_app.run(port = 8000) 
