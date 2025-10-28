1abcddf1048a538f729dab0c8a39349aa90f74f7
MobPudim <carloshkz157@gmail.com>
2025-10-27 21:48:31 -0400

    1.0.1 - resoluçaõ de fixs

--- projeto_flask/app.py
+++ projeto_flask/app.py
@@ -2,33 +2,33 @@ from flask import Flask, render_template
 from flask import request 
 from flask import jsonify 
 
-app_Mariela = Flask(__name__, template_folder='t_templates') 
+meu_app = Flask(__name__, template_folder='t_templates') 
 
-app_Mariela.config['SECRET_KEY'] = "palavra-secreta-IFRO"
+meu_app.config['SECRET_KEY'] = "palavra-secreta-IFRO"
 
-@app_Mariela.route("/")      
-@app_Mariela.route("/index")  
+@meu_app.route("/")      
+@meu_app.route("/index")  
 def indice():
     return render_template ("t_index.html") 
 
-@app_Mariela.route("/contato")
+@meu_app.route("/contato")
 def contato():
     return render_template("t_contato.html") 
 
 
-@app_Mariela.route("/usuario/<nome_usuario>;<nome_profissao>")
+@meu_app.route("/usuario/<nome_usuario>;<nome_profissao>")
 
-@app_Mariela.route("/usuario", defaults={"nome_usuario":"usuário?","nome_profissao":""})  
+@meu_app.route("/usuario", defaults={"nome_usuario":"usuário?","nome_profissao":""})  
 def usuarios (nome_usuario, nome_profissao):
     dados_usu = {"profissao": nome_profissao, "disciplina":"Desenvolvimento Web III"}
     return render_template ("t_usuario.html", nome=nome_usuario, dados = dados_usu)  
 
 
-@app_Mariela.route("/login")
+@meu_app.route("/login")
 def login():
     return render_template("t_login_flash_js_cadastro.html") 
 
-@app_Mariela.route('/teste', methods=['GET', 'POST'])
+@meu_app.route('/teste', methods=['GET', 'POST'])
 def test_route():
     if request.method == 'GET':
         return jsonify(message="Requisição GET recebida!"), 200
@@ -38,7 +38,7 @@ def test_route():
         return jsonify(error="Método não permitido"), 405
 
 
-@app_Mariela.route("/autenticar", methods=['GET','POST']) 
+@meu_app.route("/autenticar", methods=['GET','POST']) 
 def autenticar_api():
     usuario = request.form.get('nome_usuario')
     senha = request.form.get('senha')
@@ -64,14 +64,14 @@ def verificar_login(login, senha):
     else:
         return False
 
-@app_Mariela.route("/novocadastro/<nome_usuario>" , methods=['POST'])
-@app_Mariela.route("/novocadastro/", defaults={"nome_usuario":""} , methods=['POST'])
+@meu_app.route("/novocadastro/<nome_usuario>" , methods=['POST'])
+@meu_app.route("/novocadastro/", defaults={"nome_usuario":""} , methods=['POST'])
 def cadastroUsuario(nome_usuario):
     nome_usuario = request.form.get('nome_usuario')
     return render_template("t_cadastro.html", nome_login = nome_usuario ) 
 
 
 if __name__ == "__main__": 
-     app_Mariela.run(port = 8000) 
+     meu_app.run(port = 8000) 
      
      
